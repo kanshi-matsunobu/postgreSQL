0ddd77f1a2b18e993b068573495380b60ab46ff6
kanshi-matsunobu <kanshi0107@gmail.com>
2025-07-03 10:56:26 +0900

    Empty all files

--- answer/generate_test_data.sh
+++ answer/generate_test_data.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 # このファイルに回答を記述してください
-
-# ① 注文データを生成
-docker exec -i sql-batch-practice-db psql -U postgres -d practice_db -c "SELECT insert_random_orders(100);"
-
-# ② 注文明細データを生成
-docker exec -i sql-batch-practice-db psql -U postgres -d practice_db -c "SELECT insert_random_order_details();"
-
-echo "テストデータ生成が完了しました"
--- answer/run_batch.sh
+++ answer/run_batch.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
 # ここに回答を記述してください
-
-# ① バッチ集計処理
-docker exec -i sql-batch-practice-db psql -U postgres -d practice_db -c "SELECT aggregate_daily_sales();"
-
-# ② CSV出力
-docker exec -i sql-batch-practice-db psql -U postgres -d practice_db -c "\COPY daily_sales_summary TO '/tmp/daily_sales_summary.csv' WITH CSV HEADER;"
-
-# ③ コンテナからホストへコピー
-docker cp sql-batch-practice-db:/tmp/daily_sales_summary.csv ./daily_sales_summary.csv
-
-echo "バッチ処理とCSV出力が完了しました"
